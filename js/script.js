dee5ea2ec3b7b797a0627f6762c013becdc92bcd
Jessé Alves <jessealvespr@outlook.com>
2024-01-21 16:23:27 -0300

    feat: adicionado o modal

--- js/script.js
+++ js/script.js
@@ -11,3 +11,20 @@ window.onscroll = function() {
     }
   }
 
+  const modal = document.querySelector('.container-modal');
+
+addEventListener('click', (event) => {
+  if (event.target.id === 'fale-conosco') {
+    onOpen();
+  } else if (event.target.classList.contains('close')) {
+    onClose();
+  }
+})
+
+  function onClose() {
+    modal.classList.remove('active');
+  }
+
+  function onOpen() {
+    modal.classList.add('active');
+  }
