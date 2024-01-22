bbd4125bcbc6d701c49940b188a7fc8867872302
Jessé Alves <jessealvespr@outlook.com>
2024-01-22 10:47:02 -0300

    feat: realizado os requitos5 e parte do 3

--- js/script.js
+++ js/script.js
@@ -10,21 +10,3 @@ window.onscroll = function() {
       button.style.display = 'none';
     }
   }
-
-  const modal = document.querySelector('.container-modal');
-
-addEventListener('click', (event) => {
-  if (event.target.id === 'fale-conosco') {
-    onOpen();
-  } else if (event.target.classList.contains('close')) {
-    onClose();
-  }
-})
-
-  function onClose() {
-    modal.classList.remove('active');
-  }
-
-  function onOpen() {
-    modal.classList.add('active');
-  }
